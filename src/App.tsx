7b99b138a5324265249a2a8bddabd7e646d6c3a9
viktoriainga <viktoria19@ru.is>
2023-04-30 14:51:21 +0000

    Adding NavBar and SubNavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import './App.css';
-
+import NavBar from './components/NavBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavBar />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className='App-link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </a>
     </div>
   );
 }
